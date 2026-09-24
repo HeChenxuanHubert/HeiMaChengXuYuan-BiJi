5d46f426fd39786cdf498861ee9ed4f9444e6656
何宸宣 <138868740+HeChenxuanHubert@users.noreply.github.com>
2026-09-24 15:33:27 +0800

    Add files via upload

---
+++ C++核心编程/对象的初始化和清理/01 构造函数和析构函数/01 构造函数和析构函数/源.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+//对象的初始化和清理
+class Person {
+public:
+	//1.构造函数
+	//没有返回值、不用写void
+	//函数名与类的名称相同
+	//构造函数可以有参数，也可以发生重载
+	//创建对象的时候构造函数会自动调用且只调用一次
+	Person() {
+		cout << "Person构造函数的调用" << endl;
+	}
+	//2.析构函数进行清理操作
+	//没有返回值 不写void
+	//函数名与类名相同 在名称前加~
+	//析构函数不可以有参数，因此不可以发生重载
+	//对象在销毁前会自动调用析构函数，且只会调用一次
+	~Person() {
+		cout << "Person的析构函数调用" << endl;
+	}
+
+};
+//构造和析构都是必须有的实现
+//如果我们自己不提供，编译器会提供一个空实现的构造与析构
+void test01() {
+	Person p;
+}
+int main() {
+	//test01();
+	Person p;
+	system("pause");
+	return 0;
+}
---
+++ C++核心编程/对象的初始化和清理/02 构造函数的分类和调用/02 构造函数的分类和调用/源.cpp
@@ -0,0 +1,64 @@
+﻿#include<iostream>
+using namespace std;
+//1.构造函数的分类及调用
+//分类
+//按照参数分类	无参构造（默认构造）和有参构造
+//按照类型分类	普通构造和拷贝构造
+class Person {
+public:
+	//构造函数
+	Person() {
+		cout << "Person的无参构造函数调用" << endl;
+	}
+	Person(int a) {
+		age = a;
+		cout << "Person的有参构造函数调用" << endl;
+	}
+	//拷贝构造
+	Person(const Person &p) {
+		//将传入的人身上的所有属性拷贝到我身上
+		cout << "Person拷贝构造函数调用" << endl;
+
+		age = p.age;
+	}
+	int age;
+	//析构函数
+	~Person() {
+		cout << "Person的析构函数调用" << endl;
+	}
+};
+
+//调用
+void test01() {
+	//1.括号法
+	//Person p1;//默认构造函数的调用
+	//Person p2(10);//有参构造函数的调用
+	//Person p3(p2);//拷贝构造函数的调用
+	//cout << "p2的年龄为：" << p2.age << endl;
+	//cout << "p3的年龄为：" << p3.age << endl;
+	//注意事项1
+	// 调用默认构造函数的时候，不要加小括号
+	// 因为编译器会认为是一个函数的声明，不会认为在创建对象
+	
+	//2.显示法
+	//Person p1;
+	//Person p2 = Person(10);//有参构造
+	//Person p3 = Person(p2);//有参构造
+	
+	//Person(10);//匿名对象 特点：当前行执行结束后，系统会立即回收掉匿名对象
+	//cout << "aaaaa" << endl;
+	//注意事项2
+	// 不要利用拷贝构造函数初始化匿名对象
+	// 编译器会认为Person(p3)等价于Person p3,报错：重定义
+	//Person(p3);
+	
+	//3.隐式转换法
+	Person p4 = 10;// 相当于Person p4 = Person(10)
+	Person p5 = p4;// 拷贝构造
+}
+
+int main() {
+	test01();
+	system("pause");
+	return 0;
+}
